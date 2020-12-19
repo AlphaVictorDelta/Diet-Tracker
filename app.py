d2b37e011f9bc74c2a644295b41b954d3fd83de1
Adarsh Desai <desaiadarsh22@gmail.com>
2020-12-19 18:49:24 +0530

    Added Database Helpers

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask, jsonify, request, redirect, url_for, session, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+# DATABASE HELPERS 
+def connectToDatabase():
+	conn1 = sqlite3.connect('food_log.sqlite')
+	conn1.row_factory = sqlite3.Row
+	return conn1
+
+def getDatabase():
+	if not hasattr(g, 'sqlite3'):
+		g.sqlite_db = connectToDatabase()
+	return g.sqlite_db
+
+@app.teardown_appcontext
+def closeDatabaseConnection():
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
+
+
+# BASIC URL ROUTES 
 @app.route('/')
 def index():
 	return render_template('home.html')
@@ -16,5 +35,9 @@ def view_day():
 def add_food():
 	return render_template('add_food.html')
 
+
+
+
+# MAIN FUNCTION
 if __name__ == '__main__':
 	app.run()
